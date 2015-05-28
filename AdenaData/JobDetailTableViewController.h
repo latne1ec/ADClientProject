17c2f501422909e5a5b95b855b9a381d569a8cea
latne1ec <latne1ec@cmich.edu>
2015-05-28 08:20:47 -0700

    Jobs Detail Views

---
+++ AdenaData/JobDetailTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  JobDetailTableViewController.h
+//  AdenaData
+//
+//  Created by Evan Latner on 5/27/15.
+//  Copyright (c) 2015 AdenaData. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JobDetailTableViewController : UITableViewController
+
+@end
